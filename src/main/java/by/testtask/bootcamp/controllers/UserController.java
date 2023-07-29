2814d6813947aa9ce4a19eed2ff3d902a7c18690
Hanna Mikulich <mikulichhanna@gmail.com>
2023-07-29 23:55:59 +0300

    Changed UserController after adding DTO layer

--- src/main/java/by/testtask/bootcamp/controllers/UserController.java
+++ src/main/java/by/testtask/bootcamp/controllers/UserController.java
@@ -1,15 +1,14 @@
 package by.testtask.bootcamp.controllers;
 
-import by.testtask.bootcamp.models.User;
+import by.testtask.bootcamp.core.UserCreateDTO;
+import by.testtask.bootcamp.core.UserDTO;
 import by.testtask.bootcamp.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -18,13 +17,13 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/users")
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(userService.listOfUsers());
     }
 
     @PostMapping("/user")
     @ResponseStatus(HttpStatus.CREATED)
-    public void createUser(@RequestBody User user) {
+    public void createUser(@RequestBody UserCreateDTO user) {
         userService.createUser(user);
 
     }
