a9bf0515734224e114614c5e214e3493be3b9bc6
Hanna Mikulich <mikulichhanna@gmail.com>
2023-07-29 23:55:27 +0300

    Changed UserService after adding DTO layer

--- src/main/java/by/testtask/bootcamp/services/UserService.java
+++ src/main/java/by/testtask/bootcamp/services/UserService.java
@@ -1,27 +1,35 @@
 package by.testtask.bootcamp.services;
 
-import by.testtask.bootcamp.models.User;
+import by.testtask.bootcamp.core.UserCreateDTO;
+import by.testtask.bootcamp.core.UserDTO;
+import by.testtask.bootcamp.models.UserEntity;
 import by.testtask.bootcamp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final ConversionService conversionService;
 
-    public boolean createUser(User user) {
+    public boolean createUser(UserCreateDTO user) {
         String email = user.getEmail();
         if (userRepository.findByEmail(email) != null) return false;
-
-        userRepository.save(user);
+        UserEntity userEntity = conversionService.convert(user, UserEntity.class);
+        userRepository.save(userEntity);
         return true;
     }
 
+    public List<UserDTO> listOfUsers() {
+        List<UserEntity> allEntities = userRepository.findAll();
+        return allEntities.stream()
+                .map(s -> conversionService.convert(allEntities, UserDTO.class))
+                .collect(Collectors.toList());
 
-    public List<User> listOfUsers() {
-        return userRepository.findAll();
-    }
+             }
 
 }
