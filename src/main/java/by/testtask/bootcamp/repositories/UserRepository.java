1a9b224a0a8d26bc268f02ddb60c6cc91a02239e
Hanna Mikulich <mikulichhanna@gmail.com>
2023-07-30 12:12:32 +0300

    Changed UserRepository

--- src/main/java/by/testtask/bootcamp/repositories/UserRepository.java
+++ src/main/java/by/testtask/bootcamp/repositories/UserRepository.java
@@ -1,8 +1,8 @@
 package by.testtask.bootcamp.repositories;
 
 import by.testtask.bootcamp.models.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends ListCrudRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
 }
